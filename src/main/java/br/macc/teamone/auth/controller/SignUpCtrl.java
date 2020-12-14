f1d650e2952e90e4c3292d107bdc4a7afcbc7f66
Samuel Souza <samuelsouza.contact@gmail.com>
2020-12-14 13:11:02 -0300

    fixing login and signup service

--- src/main/java/br/macc/teamone/auth/controller/SignUpCtrl.java
+++ src/main/java/br/macc/teamone/auth/controller/SignUpCtrl.java
@@ -2,8 +2,10 @@ package br.macc.teamone.auth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.macc.teamone.auth.model.DefaultResponse;
@@ -12,6 +14,8 @@ import br.macc.teamone.auth.service.ISignUpService;
 
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/auth")
 public class SignUpCtrl {
 	private final ISignUpService signupService;
 	
