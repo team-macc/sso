ed8ed1af6c71b8a2be8e5d05311a5f9dd2635933
Samuel Souza <samuelsouza.contact@gmail.com>
2020-12-15 11:04:07 -0300

    new version of sso

--- src/main/java/br/macc/teamone/auth/model/ERole.java
+++
@@ -1,5 +0,0 @@
-package br.macc.teamone.auth.model;
-
-public enum ERole {
-	ROLE_CONSUMER, ROLE_VENDOR, ROLE_ADMIN
-}
--- src/main/java/br/macc/teamone/auth/service/ILoginService.java
+++
@@ -1,8 +0,0 @@
-package br.macc.teamone.auth.service;
-
-import br.macc.teamone.auth.model.Credentials;
-import br.macc.teamone.auth.model.JwtResponse;
-
-public interface ILoginService {
-	public JwtResponse login(Credentials credentials);
-}
